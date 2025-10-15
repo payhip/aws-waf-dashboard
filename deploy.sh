cbddb654ab95ad292f7e177b7a624618dd2ce4ff
“hamzaawan7” <mlkhamzaawan4210@gmail.com>
2025-10-15 18:17:48 +0500

    Revert "Implemented AWS WAF Dashboard with OpenSearch configuration and documentation"
    This reverts commit 1bab927e405339d7fb961d219ed6de1945d51717.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /Users/apple/Desktop/projects/aws-waf-dashboard
-
-# Deploy with new domain names and stack name
-cdk deploy \
-  --parameters osdfwDashboardsAdminEmail=hamzaawanit@gmail.com \
-  --parameters osdfwCognitoDomain=hamzawaf2025 \
-  --parameters osdfwOsDomainName=osdfw-opensearch-domain-2025 \
-  --require-approval never
